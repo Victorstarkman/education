01e3e1fc58e755d4a4a7f95fa07b3ce5ef0a0292
Julian Aloise <aloisejulian@gmail.com>
2022-12-26 12:17:07 -0300

    Fix show_progress

--- templates/Administration/Jobs/show_progress.php
+++ templates/Administration/Jobs/show_progress.php
@@ -1,5 +1,5 @@
 <?php $actualTime = new \Cake\I18n\FrozenTime(null,  'America/Argentina/Buenos_Aires'); ?>
-<p>Informacion se actualiza automaticamente cada 30 segundos. Ultima actualización: <?= $actualTime->format('d/m/Y H:i:s'); ?></p>
+<p>Informacion se actualiza automaticamente cada 5 segundos. Ultima actualización: <?= $actualTime->format('d/m/Y H:i:s'); ?></p>
 <table class="table table-bordered" id="tabla_actualizaciones">
 	<thead>
 	<tr>
