79c98a2578eb5076a28953b34b4d188a92467571
romulo <romulo.henrique@atlastechnol.com>
2022-12-28 10:16:53 -0300

    feat: add if

--- Service/Service/Bot/Bot.php
+++ Service/Service/Bot/Bot.php
@@ -27,7 +27,11 @@ class Bot
     public function __construct()
     {
         //set environment in php 7.4
-        include_once __DIR__ . '/Config/ConfigEnv.php';
+        $config = __DIR__ . '/Config/ConfigEnv.php';
+        if(!file_exists($config))
+            die('Config file Env not found');
+
+        include_once $config;
 
         $this->token = new Token();
         $this->user = new User();
