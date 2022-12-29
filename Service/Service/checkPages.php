e2a63e8f33fe2bb4265b480889a126cba85b37ba
romulo <romulo.henrique@atlastechnol.com>
2022-12-29 20:59:15 -0300

    up check

--- Service/Service/checkPages.php
+++ Service/Service/checkPages.php
@@ -1,7 +1,7 @@
 <?php
 
 //contar a quantidade de pasta no diretorio
-$dir=__DIR__.'/../File/Treatment/0';
+$dir=__DIR__.'/../File/Treatment/2022-12-29/0';
 $dir = scandir($dir);
 $dir = array_diff($dir, array('..', '.'));
 $dir = count($dir);
