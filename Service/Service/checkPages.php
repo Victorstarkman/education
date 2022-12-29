0e12f1a5830681e00a15b1c2decea93afe5b1cb5
romulo <romulo.henrique@atlastechnol.com>
2022-12-29 20:58:00 -0300

    frat add check

--- Service/Service/checkPages.php
+++ Service/Service/checkPages.php
@@ -1,7 +1,7 @@
 <?php
 
 //contar a quantidade de pasta no diretorio
-$dir=__DIR__.'/../File/Treatment';
+$dir=__DIR__.'/../File/Treatment/0';
 $dir = scandir($dir);
 $dir = array_diff($dir, array('..', '.'));
 $dir = count($dir);
