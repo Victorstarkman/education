e18afc475f98a43284d8ebe33fed71c2f9c2c763
Victor <victorstarkman@gmail.com>
2023-01-05 16:04:29 -0300

    modified new absent and new auditory in menu and into pages

--- templates/Administration/Patients/list_without_results.php
+++ templates/Administration/Patients/list_without_results.php
@@ -9,15 +9,15 @@
         <h4>Auditorías Pendientes</h4>
     </div>
     <div class="results">
-        <div class="mx-auto form-group row col-lg-12 col-md-12">
+       <!--  <div class="mx-auto form-group row col-lg-12 col-md-12">
             <div class="pl-0 col-6">
-                <a href="<?= $this->Url->build($this->Identity->get('groupIdentity')['redirect'] . '/nuevo-ausente', ['fullBase' => true]); ?>" class="btn btn-outline-primary col-12"><i class="mr-2 fas fa-info-circle" aria-hidden="true"></i>Nueva Auditoría</a>
+                <a href="<?//= $this->Url->build($this->Identity->get('groupIdentity')['redirect'] . '/nuevo-ausente', ['fullBase' => true]); ?>" class="btn btn-outline-primary col-12"><i class="mr-2 fas fa-info-circle" aria-hidden="true"></i>Nueva Auditoría</a>
             </div>
             <div class="pl-0 col-6">
-                <a href="<?= $this->Url->build($this->Identity->get('groupIdentity')['redirect'] . '/nuevo-agente', ['fullBase' => true]); ?>" class="btn btn-outline-primary col-12"><i class="mr-2 fas fa-info-circle" aria-hidden="true"></i>Nuevo Agente</a>
+                <a href="<?//= $this->Url->build($this->Identity->get('groupIdentity')['redirect'] . '/nuevo-agente', ['fullBase' => true]); ?>" class="btn btn-outline-primary col-12"><i class="mr-2 fas fa-info-circle" aria-hidden="true"></i>Nuevo Agente</a>
             </div>
 
-        </div><!-- fin de row -->
+        </div><!-- fin de row --> 
         <p class="title-results">Auditorías</p>
 
         <?= $this->Flash->render() ?>
